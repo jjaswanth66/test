07888e8a490fd3a1c3457c588d90d263c1b3dc6d
jjaswanth66 <118841365+jjaswanth66@users.noreply.github.com>
2023-02-03 17:51:44 +0530

    Add files via upload

---
+++ src/main/java/org/springframework/boot/spring_boot_starter_tests/App.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.spring_boot_starter_tests;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/org/springframework/boot/spring_boot_starter_tests/AppTest.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.spring_boot_starter_tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
